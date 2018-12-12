f75b8449c0caf0715c85c8fa4fdfd5eaef854cbb
patmmm <patmmmmmmmmmmmm@gmail.com>
2018-12-12 12:31:35 +0700

    layout , search and report page

--- navigation.js
+++ navigation.js
@@ -4,17 +4,19 @@ import React, { Component } from 'react';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import PageHome from './pageHome';
 import UncheckedItem from './uncheckedItem';
+import Report from './reportBrokenBarcode';
 
 // -------------- Navigation Stack -----------------
 const RootStack = createStackNavigator(
     {
         //all pages for navigation
+        Report: Report,
         Home: PageHome,
         UncheckedItem: UncheckedItem,
     },
     {
         //starting page
-        initialRouteName: 'Home',
+        initialRouteName: 'UncheckedItem',
     }
 );
 
--- uncheckedItem.js
+++ uncheckedItem.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, FlatList } from 'react-native'
+import { Text, StyleSheet, View, Button, FlatList, TextInput, TouchableOpacity } from 'react-native'
 import { Icon } from "native-base";
-import { createStackNavigator } from 'react-navigation';
+import Modal from "react-native-modal";
+import {
+    Col,
+    Row,
+    Grid
+} from "react-native-easy-grid";
 export default class UncheckedItem extends Component {
     constructor(props) {
         super(props)
@@ -38,8 +43,13 @@ export default class UncheckedItem extends Component {
                     icon: { collection: 'MaterialCommunityIcons', name: 'file-check' },
                     status: 'ครุภัณฑ์ปกติ',
                 }
-            ]
+            ],
+            searchText: '',
+            modalVisabled: false
         }
+        // this.searchData('')
+        // this.state = { show: this.state.data }
+
     }
 
 
@@ -54,27 +64,102 @@ export default class UncheckedItem extends Component {
         },
     }
 
+    searchData = (text) => {
+        show = this.state.data.filter((element) => element.key.startsWith(text))
+        this.setState({ show: show });
+    }
+
+    viewDetail = (element) => {
+        this.setState({ modalVisabled: element });
+    }
 
     render() {
         return (
             <View>
-                <Text> textInComponent </Text>
-                <FlatList
-                    data={this.state.data}
-                    renderItem={({ item }) =>
-                        <View style={styles.listContainer}>
-                            <View style={styles.itemList}>
-                                <Text> {item.key} </Text>
-                                <Text> {item.name} </Text>
-                            </View>
-                            <View style={styles.itemList}>
-                                <Text> สถานะ </Text>
-                                <Icon name={item.icon.name} type={item.icon.collection} />
-                                <Text> {item.status} </Text>
-                            </View>
+                <View style={{ borderBottomWidth: 2, borderStyle: "solid", borderColor: 'black' }}>
+                    <View style={{ flex: 0, paddingHorizontal: 10, margin: 10, height: 40, flexDirection: "row", alignItems: "center", borderStyle: 'solid', borderWidth: 2, borderColor: 'black', borderRadius: 10 }}>
+                        <Icon name="md-search" type="Ionicons" />
+                        <View style={{ flex: 1 }}>
+                            <TextInput placeholder='search' value={this.state.searchText}
+                                onChangeText={(text) => {
+                                    this.setState({ searchText: text })
+                                    this.searchData(text)
+                                }
+                                } />
                         </View>
+                    </View>
+                </View>
+                {/* <Text>{this.state.searchText}</Text> */}
+                <FlatList
+                    data={this.state.show}
+                    renderItem={({ item, index }) =>
+                        <TouchableOpacity onPress={() => this.viewDetail(item)}>
+                            <View style={styles.listContainer}>
+                                <View style={styles.itemList}>
+                                    <Text> {item.key} </Text>
+                                    <Text> {item.name} </Text>
+                                </View>
+                                <View style={styles.itemList}>
+                                    <Text> สถานะ </Text>
+                                    <Icon name={item.icon.name} type={item.icon.collection} />
+                                    <Text> {item.status} </Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
                     }
                 />
+                
+                <Modal
+                    animationIn="fadeIn"
+                    animationOut="fadeOut"
+                    isVisible={(this.state.modalVisabled) ? true : false}
+                    onBackdropPress={() => this.setState({ modalVisabled: false })}
+                    style={{ alignItems: 'center' }}
+                >
+                    <View style={{ backgroundColor: 'white', padding: 10, borderStyle: 'solid', width: 300, borderWidth: 2, borderColor: 'black' }}>
+                        <Text>ข้อมูลครุภัณฑ์</Text>
+                        <View
+                            style={{
+                                borderBottomColor: 'black',
+                                borderBottomWidth: 1,
+                                margin: 7
+                            }}
+                        />
+                        <View style={{ flex: 0, height: 120 }}>
+
+                            <Grid>
+                                <Row>
+                                    <Col><Text>รหัส </Text></Col>
+                                    <Col><Text>{this.state.modalVisabled.key} </Text></Col>
+                                </Row>
+                                <Row>
+                                    <Col><Text>ชื่อ </Text></Col>
+                                    <Col><Text>{this.state.modalVisabled.name} </Text></Col>
+                                </Row>
+                                <Row>
+                                    <Col><Text>หน่วยงาน </Text></Col>
+                                    <Col><Text>{this.state.modalVisabled.name} </Text></Col>
+                                </Row>
+                                <Row>
+                                    <Col><Text>สถานที่ตั้ง </Text></Col>
+                                    <Col><Text>{this.state.modalVisabled.name} </Text></Col>
+                                </Row>
+                                <Row>
+                                    <Col><Text>สถานะ </Text></Col>
+                                    <Col><Text>{this.state.modalVisabled.status} </Text></Col>
+                                </Row>
+                            </Grid>
+                        </View>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                            <Button title="ยกเลิก" onPress={() => this.setState({ modalVisabled: false })} />
+                            <Button title="แจ้ง Barcode ชำรุด" onPress={() => {
+                                temp = JSON.stringify(this.state.modalVisabled)
+                                this.setState({ modalVisabled: false })
+                                this.props.navigation.navigate('Report', { item: JSON.parse(temp) })
+                            }} />
+                        </View>
+                    </View>
+                </Modal>
             </View>
 
         )
@@ -82,14 +167,15 @@ export default class UncheckedItem extends Component {
 }
 
 const styles = StyleSheet.create({
-  itemList:{
-    flexDirection:'row'
-  },
-  listContainer:{
-    flex:1,
-    borderStyle: 'solid',
-    borderBottomColor:'black',
-    borderBottomWidth: 1,
-    // backgroundColor:'red'
-  }
+    itemList: {
+        flexDirection: 'row'
+    },
+    listContainer: {
+        flex: 1,
+        borderStyle: 'solid',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        padding: 5
+        // backgroundColor:'red'
+    }
 })
