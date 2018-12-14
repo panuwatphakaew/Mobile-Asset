2a26b7099e198f7191260e6783c854575780beae
Miracle <mgta_7460@hotmail.com>
2018-12-14 12:05:07 +0700

    Revert "no message"
    This reverts commit 0d8957b1650d836582758b88193e3dea742bbda3.

--- NormalEmployees.js
+++ NormalEmployees.js
@@ -3,9 +3,6 @@ import { Text, Image, View, StyleSheet } from 'react-native';
 import { Icon } from 'native-base';
 
 export default class NormalEmployee extends Component {
-  static navigationOptions = {
-    header:null
-}
   render() {
     return (
       <View style={{flex: 1, backgroundColor: 'white'}}>
